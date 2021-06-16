1c3ac8131427d265d512693f192f25f0782a8e7a
taliayit <taliayit@gmail.com>
2021-06-16 15:37:29 +0300

    force LTR layout for RTL devices

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Parse from "parse/react-native.js";
@@ -8,9 +6,14 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Farm from './pages/Farm';
+import { I18nManager } from "react-native";
 
 export default function App() {
-
+  // force LTR layout for RTL devices
+  I18nManager.forceRTL(false);
+  I18nManager.allowRTL(false);
+  
+  // initialize parse SDK 
   Parse.serverURL = 'https://parseapi.back4app.com/';
   Parse.setAsyncStorage(AsyncStorage);
   Parse.initialize('Z2GjdTOFqHoydCPICvNH6kannQnF2WTwh3KjdPvx','N9sQIz454ccnF39FHURYy3C5GVzGIp6bYK7fpPAw');
